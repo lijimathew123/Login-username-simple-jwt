312f5ab68e16dab258c8cba45f222fd2b936b511
Liji Mathew <107242364+lijimathew123@users.noreply.github.com>
2024-03-05 11:14:08 +0530

    Update models.py

--- registration/myapp/models.py
+++ registration/myapp/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.hashers import make_password 
 
 class Employee(models.Model):
+    username=models.CharField(max_length=30,default="")
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128) 
 
 
     def save(self, *args, **kwargs):
-        # Hash the password before saving
+        # Use make_password to hash the password
         self.password = make_password(self.password)
         super().save(*args, **kwargs)
 
